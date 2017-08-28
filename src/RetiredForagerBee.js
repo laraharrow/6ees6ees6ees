57f91938be584825ff1550a111c1fb4ab7c36bec
laraismael1 <laraismael123@gmail.com>
2017-08-28 12:25:48 -0700

    implemented RetiredForagerBee class

--- src/RetiredForagerBee.js
+++ src/RetiredForagerBee.js
@@ -1,3 +1,13 @@
-class RetiredForagerBee {
-  // TODO..
+class RetiredForagerBee extends ForagerBee {
+  constructor() {
+  	super();
+  	this.age = 40;
+  	this.color = 'grey';
+  	this.job = 'gamble';
+  	this.canFly = false;
+
+  	this.forage = () => 'I am too old, let me play cards instead';
+    this.gamble = () => this.treasureChest.push('treasure');
+
+  }
 };
